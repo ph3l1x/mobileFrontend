24b1b2ab4ea53c2d3900aad0cd81d795dfcdeabe
ph3l1x <rperry@mindimage.net>
2015-10-06 00:00:34 -0600

    Auth

---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,71 @@
+angular.module('starter.controllers', [])
+
+    .controller('LoginCtrl', function($scope, $state, $ionicPopup, AuthService) {
+        $scope.data = {};
+
+        $scope.login = function(data) {
+            AuthService.login(data.username, data.password).then(function(authenticated) {
+                $state.go('main.dash', {}, {reload: true});
+                $scope.setCurrentUsername(data.username);
+            }, function(err) {
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Login failed!',
+                    template: 'Please check your credentials!'
+                });
+            });
+        };
+    })
+    .controller('DashCtrl', function($scope, $state, $http, $ionicPopup, AuthService) {
+        $scope.logout = function() {
+            AuthService.logout();
+            $state.go('login');
+        };
+
+        $scope.performValidRequest = function() {
+            $http.get('http://db.copz.net').then(
+                function(result) {
+                    $scope.response = result;
+                });
+        };
+
+        $scope.performUnauthorizedRequest = function() {
+            $http.get('http://db.copz.net').then(
+                function(result) {
+                    // No result here..
+                }, function(err) {
+                    $scope.response = err;
+                });
+        };
+
+        $scope.performInvalidRequest = function() {
+            $http.get('http://db.copz.net').then(
+                function(result) {
+                    // No result here..
+                }, function(err) {
+                    $scope.response = err;
+                });
+        };
+    })
+    .controller('AppCtrl', function($scope, $state, $ionicPopup, AuthService, AUTH_EVENTS) {
+        $scope.username = AuthService.username();
+
+        $scope.$on(AUTH_EVENTS.notAuthorized, function(event) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Unauthorized!',
+                template: 'You are not allowed to access this resource.'
+            });
+        });
+
+        $scope.$on(AUTH_EVENTS.notAuthenticated, function(event) {
+            AuthService.logout();
+            $state.go('login');
+            var alertPopup = $ionicPopup.alert({
+                title: 'Session Lost!',
+                template: 'Sorry, You have to login again.'
+            });
+        });
+
+        $scope.setCurrentUsername = function(name) {
+            $scope.username = name;
+        };
+    });
