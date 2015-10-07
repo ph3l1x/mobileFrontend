7a035f6fbfc52ab4b2325f754f53e8b07ff17e9c
ph3l1x <rperry@mindimage.net>
2015-10-06 23:41:49 -0600

    test

--- www/js/app.js
+++ www/js/app.js
@@ -17,94 +17,25 @@ angular.module('mobileApp', ['ionic', 'mobileApp.controllers', 'ui.router', 'sat
         });
     })
 
-    .config(function ($stateProvider, $urlRouterProvider, $authProvider, USER_ROLES) {
+    .config(function($stateProvider, $urlRouterProvider, $authProvider) {
 
-        $authProvider.loginUrl = 'db.copz.net/api/authenticate';
+        // Satellizer configuration that specifies which API
+        // route the JWT should be retrieved from
+        $authProvider.loginUrl = '/api/authenticate';
+
+        // Redirect to the auth state if any other states
+        // are requested other than users
         $urlRouterProvider.otherwise('/auth');
 
         $stateProvider
             .state('auth', {
                 url: '/auth',
-                templateUrl: 'templates/authView.html',
+                templateUrl: '../views/authView.html',
                 controller: 'AuthController as auth'
             })
             .state('users', {
                 url: '/users',
-                templateUrl: 'templates/userView.html',
+                templateUrl: '../views/userView.html',
                 controller: 'UserController as user'
-            })
-            .state('login', {
-                url: '/login',
-                templateUrl: 'templates/login.html',
-                controller: 'LoginCtrl'
-            })
-            .state('main', {
-                url: '/',
-                abstract: true,
-                templateUrl: 'templates/main.html'
-            })
-            .state('main.dash', {
-                url: 'main/dash',
-                views: {
-                    'dash-tab': {
-                        templateUrl: 'templates/dashboard.html',
-                        controller: 'DashCtrl'
-                    }
-                }
-            })
-            .state('main.public', {
-                url: 'main/public',
-                views: {
-                    'public-tab': {
-                        templateUrl: 'templates/public.html'
-                    }
-                }
-            })
-            .state('main.admin', {
-                url: 'main/admin',
-                views: {
-                    'admin-tab': {
-                        templateUrl: 'templates/admin.html'
-                    }
-                },
-                data: {
-                    authorizedRoles: [USER_ROLES.admin]
-                }
             });
-        $urlRouterProvider.otherwise(function ($injector, $location) {
-            var $state = $injector.get("$state");
-            $state.go("main.dash");
-        });
-    })
-
-    .run(function($httpBackend){
-        //$httpBackend.whenGET('http://db.xxx.local')
-        //    .respond({message: 'This is my valid response!'});
-        //$httpBackend.whenGET('http://db.xxx.local')
-        //    .respond(401, {message: "Not Authenticated"});
-        //$httpBackend.whenGET('http://db.xxx.local')
-        //    .respond(403, {message: "Not Authorized"});
-        //
-        //$httpBackend.whenGET(/templates\/\w+.*/).passThrough();
-    })
-
-    .run(function ($rootScope, $state, AuthService, AUTH_EVENTS) {
-        $rootScope.$on('$stateChangeStart', function (event,next, nextParams, fromState) {
-
-            if ('data' in next && 'authorizedRoles' in next.data) {
-                var authorizedRoles = next.data.authorizedRoles;
-                if (!AuthService.isAuthorized(authorizedRoles)) {
-                    event.preventDefault();
-                    $state.go($state.current, {}, {reload: true});
-                    $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
-                }
-            }
-
-            if (!AuthService.isAuthenticated()) {
-                if (next.name !== 'login') {
-                    event.preventDefault();
-                    $state.go('login');
-                }
-            }
-        });
     });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,86 +1,29 @@
 angular
-    .module('mobileApp.controllers', [])
-    .controller('AuthController', function($auth, $state) {
-        var vm = this;
-        m.login = function() {
-            var credentials = {
-                email: vm.email,
-                password: vm.password
-            }
-            $auth.login(credentials).then(function(data) {
-                //If login is successful, redirectto the users state
-                $state.go('users', {});
-            });
+    .module('mobileApp')
+    .controller('AuthController', AuthController);
+
+
+function AuthController($auth, $state) {
+
+    var vm = this;
+
+    vm.login = function() {
+
+        var credentials = {
+            email: vm.email,
+            password: vm.password
         }
-    })
-    .controller('LoginCtrl', function($scope, $state, $ionicPopup, AuthService) {
-        $scope.data = {};
 
-        $scope.login = function(data) {
-            AuthService.login(data.username, data.password).then(function(authenticated) {
-                $state.go('main.dash', {}, {reload: true});
-                $scope.setCurrentUsername(data.username);
-            }, function(err) {
-                var alertPopup = $ionicPopup.alert({
-                    title: 'Login failed!',
-                    template: 'Please check your credentials!'
-                });
-            });
-        };
-    })
-    .controller('DashCtrl', function($scope, $state, $http, $ionicPopup, AuthService) {
-        $scope.logout = function() {
-            AuthService.logout();
-            $state.go('login');
-        };
+        // Use Satellizer's $auth service to login
+        $auth.login(credentials).then(function(data) {
 
-        $scope.performValidRequest = function() {
-            $http.get('http://db.copz.net').then(
-                function(result) {
-                    $scope.response = result;
-                });
-        };
-
-        $scope.performUnauthorizedRequest = function() {
-            $http.get('http://db.copz.net').then(
-                function(result) {
-                    // No result here..
-                }, function(err) {
-                    $scope.response = err;
-                });
-        };
-
-        $scope.performInvalidRequest = function() {
-            $http.get('http://db.copz.net').then(
-                function(result) {
-                    // No result here..
-                }, function(err) {
-                    $scope.response = err;
-                });
-        };
-    })
-    .controller('AppCtrl', function($scope, $state, $ionicPopup, AuthService, AUTH_EVENTS) {
-        $scope.username = AuthService.username();
-
-        $scope.$on(AUTH_EVENTS.notAuthorized, function(event) {
-            var alertPopup = $ionicPopup.alert({
-                title: 'Unauthorized!',
-                template: 'You are not allowed to access this resource.'
-            });
+            // If login is successful, redirect to the users state
+            $state.go('users', {});
         });
+    }
 
-        $scope.$on(AUTH_EVENTS.notAuthenticated, function(event) {
-            AuthService.logout();
-            $state.go('login');
-            var alertPopup = $ionicPopup.alert({
-                title: 'Session Lost!',
-                template: 'Sorry, You have to login again.'
-            });
-        });
+}
 
-        $scope.setCurrentUsername = function(name) {
-            $scope.username = name;
-        };
-    });
+
 
 
