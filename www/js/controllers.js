202005964f2c635822b7e1efb5e14b80f0f7be93
ph3l1x <rperry@mindimage.net>
2015-10-08 21:49:08 -0600

    Regisration Form Work

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,10 +3,44 @@ angular
 
 
     .controller('AuthController', AuthController)
-    .controller('RegisterController', RegisterController);
+    .controller('RegisterController', RegisterController)
+    .controller('UserController', UserController);
 
-function RegisterController($auth, $scope) {
+function RegisterController($auth, $scope, $http, $ionicPopup, $state) {
+    console.log("REGISTER CONTROLLER");
+    $scope.registration = function() {
+        $http.get("http://").then(
+            function(result) {
+                $scope.response = result;
+            });
+    };
+}
 
+function UserController($http) {
+
+    var vm = this;
+
+    vm.users;
+    vm.error;
+    vm.something;
+
+    vm.getUsers = function() {
+
+        // This request will hit the index method in the AuthenticateController
+        // on the Laravel side and will return the list of users
+        $http.get('http://db.copz.net/api/authenticate').success(function(users) {
+            vm.users = users;
+        }).error(function(error) {
+            vm.error = error;
+        });
+    };
+    vm.doSomething = function() {
+        $http.get('http://db.copz.net/shit').success(function(something) {
+            vm.something = something;
+        }).error(function(error) {
+            vm.error = error;
+        });
+    }
 }
 
 function AuthController($auth, $state) {
